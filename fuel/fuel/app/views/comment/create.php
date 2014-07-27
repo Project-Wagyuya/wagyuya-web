de6c9375555420553dfd427270412f7ed5acf849
masato-wada <mw.d0.0b.wm@gmail.com>
2014-07-28 00:04:10 +0900

    +7/27に追加コミット

---
+++ fuel/fuel/app/views/comment/create.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>コメント内容</title>
+</head>
+<body>
+
+<h1>
+	質問内容
+</h1>
+<b><? echo $question->title; ?></b><br>
+<? echo $question->desc; ?><br>
+<?php
+foreach ($selectList as $select)
+{
+?>
+	<h3><? echo $select->desc; ?></h3>
+<?php
+}
+?>
+<br>
+
+<h1>
+	コメント
+</h1>
+
+<?php
+	echo Form::open(array('action'=>'comment/commit' ,'method'=>'post'));
+
+	//コメント入力欄 
+	echo Form::textarea('comment','コメントを入力ください',array('rows'=>3));
+	echo '<br>';
+
+	//submit
+	echo Form::input('send','コメントを投稿',array('type'=>'submit'));
+
+	//hidden
+	echo Form::hidden('question_id',$question->id,array());
+
+	echo Form::close();
+?>
+
+
+</body>
---
+++ fuel/fuel/app/views/comment/detail.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>コメント内容</title>
+</head>
+<body>
+
+<h1>
+	質問内容
+</h1>
+<b><? echo $question->title; ?></b><br>
+<? echo $question->desc; ?><br>
+<?php
+foreach ($selectList as $select)
+{
+?>
+	<h3><? echo $select->desc; ?></h3>
+<?php
+}
+?>
+<br>
+<?php echo \Fuel\Core\Html::anchor('/q/' . $question->id, 'アンケートに投票する');?><br>
+<?php echo \Fuel\Core\Html::anchor('/c_create/' . $question->id, 'アンケートにコメントする');?><br>
+
+
+<h1>
+	コメント
+</h1>
+<?php
+foreach ($commentList as $comment)
+{
+?>
+	<li>
+		<? echo $comment['created_at']; ?><br>
+		<?
+			$c = Format::forge($comment)->to_array();
+			if($c['select_id']){
+				echo $selectList_byArray[$c['select_id']].'派<br>';
+			};
+		?>
+		<? echo $comment['comment']; ?><br>
+	</li><br>
+<?php
+}
+?>
+
+
+</body>
